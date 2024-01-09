4473e60b77c566893ca4e258b70beed76ebc6059
Hibiz3a <106378987+Hibiz3a@users.noreply.github.com>
2024-01-09 13:41:15 +0100

    end of mouvement player

---
+++ Assets/Script/Mouvement/Pl_Jump.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    [Range(0f, 100f)]
+    public float jumpStrength;
+    public event System.Action Jumped;
+
+
+    public Animator playerAnim;
+
+    [SerializeField, Tooltip("Prevents jumping when the transform is in mid-air.")]
+    GroundCheck groundCheck;
+
+
+    void Reset()
+    {
+        // Try to get groundCheck.
+        playerAnim = GetComponent<Animator>();
+        groundCheck = GetComponentInChildren<GroundCheck>();
+    }
+
+    void Awake()
+    {
+        // Get rigidbody.
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void LateUpdate()
+    {
+        playerAnim.SetBool("crouch", !groundCheck.isGrounded);
+        // Jump when the Jump button is pressed and we are on the ground.
+        if (Input.GetButtonDown("Jump") && (!groundCheck || groundCheck.isGrounded))
+        {
+            rigidbody.AddForce(Vector3.up * jumpStrength * 10);
+            Jumped?.Invoke();
+        }
+    }
+}
--- Assets/Script/Mouvement/Pl_PlayerMotor.cs
+++ Assets/Script/Mouvement/Pl_PlayerMotor.cs
@@ -4,13 +4,22 @@ using UnityEngine.Animations;
 [RequireComponent (typeof(Rigidbody))]
 public class Pl_PlayerMotor : MonoBehaviour
 {
-    public Vector3 velocity;
+    private Vector3 velocity;
+    private Vector3 rotation;
+    private Vector3 rotationCamera;
+
+    [SerializeField]
+    private Camera cam;
+
+
     private Rigidbody rb;
 
     [SerializeField] Animator walk;
 
     private void Start()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         walk = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
     }
@@ -23,6 +32,7 @@ public class Pl_PlayerMotor : MonoBehaviour
     private void FixedUpdate()
     {
         performMouvement();
+        performRotation();
 
         walk.SetFloat("walk", Input.GetAxisRaw("Vertical"));
         walk.SetFloat("side", Input.GetAxisRaw("Horizontal"));
@@ -34,5 +44,22 @@ public class Pl_PlayerMotor : MonoBehaviour
         {
             rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
         }
+    }
+
+    private void performRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+
+        cam.transform.Rotate(-rotationCamera);
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(Vector3 _rotationCamera)
+    {
+        rotationCamera = _rotationCamera;
     }
 }
